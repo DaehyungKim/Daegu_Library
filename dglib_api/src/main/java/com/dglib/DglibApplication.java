388ce9e2e97b294dba02a69d9c32474c566f7793
baek <bsm100100@gmail.com>
2025-05-02 17:45:28 +0900

    new

---
+++ dglib_api/src/main/java/com/dglib/DglibApplication.java
@@ -0,0 +1,13 @@
+package com.dglib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DglibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DglibApplication.class, args);
+	}
+
+}
