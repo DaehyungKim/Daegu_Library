db79f44e0cebd10786b3b58419ba834b63c6cefd
baek <bsm100100@gmail.com>
2025-05-07 12:25:48 +0900

    Merge branch 'member' of https://github.com/1Hunnit-Square/DaeguLibrary into member
    # Conflicts:
    #	dglib_api/src/main/resources/application.properties

--- dglib_api/src/main/java/com/dglib/dto/MemberDTO.java
+++ dglib_api/src/main/java/com/dglib/dto/MemberDTO.java
@@ -1,5 +1,19 @@
 package com.dglib.dto;
 
-public class MemberDTO {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class MemberDTO extends User{
+	
+	public MemberDTO(String id, Collection<? extends GrantedAuthority> authorities) {
+		super(id, null, authorities);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String id;
+	
+
 
 }
